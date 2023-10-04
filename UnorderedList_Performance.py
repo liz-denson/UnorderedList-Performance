b883fb3d4553037f79633de1f400cce470de1c75
liz-denson <123184723+liz-denson@users.noreply.github.com>
2023-10-04 15:09:10 -0400

    Update UnorderedList_Performance.py
    Main function draft

--- UnorderedList_Performance.py
+++ UnorderedList_Performance.py
@@ -13,7 +13,6 @@ import timeit
 ###########
 # FUNCTIONS
 ###########
-
 def create_ul():
   ul = UnorderedList()
   for _ in range(25000):
@@ -35,13 +34,31 @@ def delete_pl(pl):
 ######
 # MAIN
 ######
-
 def main():
-  # Part I: Creation
-  
-  # Part II: Deletion
-  
-  
+    """Create Timing"""
+    ul_create_time = timeit.timeit("create_ul()", "from __main__ import create_ul", number=100)
+    pl_create_time = timeit.timeit("create_pl()", "from __main__ import create_pl", number=100)
+
+    print(f"Creating:")
+    print(f"UnorderedList: {ul_create_time:.3f}s")
+    print(f"Python List: {pl_create_time:.3f}s")
+
+    """Delete Timing"""
+    ul = create_ul()
+    pl = create_pl()
+
+    start_time = time.time()
+    delete_ul(ul)
+    ul_delete_time = time.time() - start_time
+
+    start_time = time.time()
+    delete_pl(pl)
+    pl_delete_time = time.time() - start_time
+
+    print(f"\nDeleting:")
+    print(f"UnorderedList: {ul_delete_time:.3f}s")
+    print(f"Python List: {pl_delete_time:.3f}s")
+
 if __name__ == "__main__":
     main()
 
